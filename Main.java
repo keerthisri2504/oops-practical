72a541875706c00506d791cd47eee249dcb02df5
keerthisri2504 <nandyalakeerthisri@gmail.com>
2025-08-26 12:11:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package oops_practical;
+
+interface Animal {
+	  public void animalSound(); // interface method
+	  public void sleep(); // interface method (does not have a body)
+	}
+	// Dog "implements" the Animal interface
+	class Dog implements Animal {
+	  public void animalSound() {
+	    // The body of animalSound() is provided here
+	    System.out.println("The Dog says:Bow Bow..");
+	  }
+	  public void sleep() {
+	    // The body of sleep() is provided here
+	    System.out.println("Zzz");
+	  }
+	}
+	class Main {
+		  public static void main(String[] args) {
+		    Dog myDog = new Dog();  // Create a Dog object
+		    myDog.animalSound();
+		    myDog.sleep();
+		  }
+		}
---
+++ Multiple.java
@@ -0,0 +1,32 @@
+package oops_practical;
+
+import java.io.*;
+//Add interface
+interface Add{
+ int add(int a,int b);
+}
+interface Sub{
+	int sub(int a,int b);
+}
+class Cal implements Add , Sub
+{
+	// Method to add two numbers
+	public int add(int a,int b){
+   	return a+b; }
+	// Method to sub two numbers
+	public int sub(int a,int b){
+ 	return a-b;
+}   	 }
+
+class Multiple{
+    // Main Method
+    public static void main (String[] args) 
+    {
+      	// instance of Cal class
+      	Cal x = new Cal();
+      
+      	System.out.println("Addition : " + x.add(2,1));
+      	System.out.println("Substraction : " + x.sub(2,1));
+      
+    }
+}
